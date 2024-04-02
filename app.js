3ca20e350cb8471ba50c7098cc40758fe430a7fb
Arslan <muhammadarslanrajput12@gmail.com>
2024-04-01 20:45:54 -0700

    Javascript

---
+++ app.js
@@ -0,0 +1,10 @@
+let password=246810
+let passwordUser=+prompt("Type your password in number")
+if(password===passwordUser){
+    document.write("Right password")
+ 
+}
+else if(passwordUser===0){
+    document.write("please type password ")
+}
+else{document.write("try again")}
